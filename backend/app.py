4daf37db1015f3232526b8c66531a355ce62d63e
Yash26-112003 <YP76179@GMAIL.COM>
2026-04-14 00:36:05 +0530

    app.py

--- backend/app.py
+++ backend/app.py
@@ -98,4 +98,4 @@ def upload():
 
 if __name__ == "__main__":
     print("🚀 Backend Starting...")
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=10000)
